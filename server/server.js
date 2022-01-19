31286cf5fa4a915372f32d3f4740f424d271867c
Josh W <sdkjqg@gmail.com>
2022-01-18 21:32:32 -0800

    dev branch added

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import config from './../config/config'
+import app from './express'
+import mongoose from 'mongoose'
+
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose
+  .connect(
+   config.mongoUri, 
+    { useNewUrlParser: true }
+  ) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', config.port)
+})
