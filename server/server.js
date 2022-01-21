d3a5aa4b4a56ed61f2dbbd0f5c3a2a115dfab778
Josh W <sdkjqg@gmail.com>
2022-01-20 18:19:42 -0800

    mongodb fixed dev branch

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-import config from './../config/config'
-import app from './express'
-import mongoose from 'mongoose'
+import config from "./../config/config";
+import app from "./express";
+import mongoose from "mongoose";
 
 // Connection URL
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 mongoose
-  .connect(
-   config.mongoUri, 
-    { useNewUrlParser: true }
-  ) // Adding new mongo url parser
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
-
+  .connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }) // Adding new mongo url parser
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.listen(config.port, (err) => {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
-  console.info('Server started on port %s.', config.port)
-})
+  console.info("Server started on port %s.", config.port);
+});
